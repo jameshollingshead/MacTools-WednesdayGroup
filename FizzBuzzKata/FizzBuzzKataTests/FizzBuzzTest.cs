e63e22e0020e7462dfc2149d3daa055a88e22ea5
James Hollingshead <git@jameshollingshead.com>
2017-07-31 13:28:53 -0400

    Initial FizzBuzz kata setup

---
+++ FizzBuzzKata/FizzBuzzKataTests/FizzBuzzTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FizzBuzzKata;
+using NUnit.Framework;
+
+namespace FizzBuzzKataTests
+{
+    [TestFixture]
+    public class FizzBuzzTest
+    {
+        private FizzBuzz fizzBuzz;
+
+        [SetUp]
+        public void SetUp()
+        {
+            fizzBuzz = new FizzBuzz();
+        }
+
+        
+    }
+}
