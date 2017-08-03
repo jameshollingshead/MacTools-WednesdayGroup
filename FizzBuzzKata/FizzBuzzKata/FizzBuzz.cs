e11e37d4ca0772b6f35977a8805dc33766e3a462
James Hollingshead <git@jameshollingshead.com>
2017-08-03 11:12:28 -0400

    FizzBuzz kata completed

--- FizzBuzzKata/FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzzKata/FizzBuzz.cs
@@ -8,6 +8,36 @@ namespace FizzBuzzKata
 {
     public class FizzBuzz
     {
-        
+        public string GetFizzBuzz(int input)
+        {
+            var result = input.ToString();
+
+            if (IsDivisibleBy3(input))
+                result = "Fizz";
+
+            if (IsDivisibleBy5(input))
+                result = "Buzz";
+
+            if (IsDivisibleBy15(input))
+                result = "FizzBuzz";
+
+
+            return result;
+        }
+
+        private static bool IsDivisibleBy15(int input)
+        {
+            return input % 15 == 0;
+        }
+
+        private static bool IsDivisibleBy5(int input)
+        {
+            return input % 5 == 0;
+        }
+
+        private static bool IsDivisibleBy3(int input)
+        {
+            return input % 3 == 0;
+        }
     }
 }
--- FizzBuzzKata/FizzBuzzKataTests/FizzBuzzTest.cs
+++ FizzBuzzKata/FizzBuzzKataTests/FizzBuzzTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using FizzBuzzKata;
@@ -19,6 +20,61 @@ namespace FizzBuzzKataTests
             fizzBuzz = new FizzBuzz();
         }
 
-        
+        [Test]
+        public void GivenAnInputOf1TheFunctionReturns1()
+        {
+            var result = fizzBuzz.GetFizzBuzz(1);
+            Assert.AreEqual("1", result);
+        }
+
+        [Test]
+        public void GivenAnInputOf2TheFunctionReturns2()
+        {
+            var result = fizzBuzz.GetFizzBuzz(2);
+            Assert.AreEqual("2", result);
+        }
+
+        [Test]
+        public void GivenAnInputOf3TheFunctionReturnsFizz()
+        {
+            var result = fizzBuzz.GetFizzBuzz(3);
+            Assert.AreEqual("Fizz", result);
+        }
+
+        [Test]
+        public void GivenAnInputOf5TheFunctionReturnsBuzz()
+        {
+            var result = fizzBuzz.GetFizzBuzz(5);
+            Assert.AreEqual("Buzz", result);
+        }
+
+        [Test]
+        public void GivenAnInputOf6TheFunctionReturnsFizz()
+        {
+            var result = fizzBuzz.GetFizzBuzz(6);
+            Assert.AreEqual("Fizz", result);
+        }
+
+        [Test]
+        public void GivenAnInputOf10TheFunctionReturnsBuzz()
+        {
+            var result = fizzBuzz.GetFizzBuzz(10);
+            Assert.AreEqual("Buzz", result);
+        }
+
+        [Test]
+        public void GivenAnInputOf15TheFunctionReturnsFizzBuzz()
+        {
+            var result = fizzBuzz.GetFizzBuzz(15);
+            Assert.AreEqual("FizzBuzz", result);
+        }
+
+        [Test]
+        public void GivenAnInputOf30TheFunctionReturnsFizzBuzz()
+        {
+            var result = fizzBuzz.GetFizzBuzz(30);
+            Assert.AreEqual("FizzBuzz", result);
+        }
+
     }
 }
