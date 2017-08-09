dc8e9b1e98f5126071c60a453570004d80bd9623
James Hollingshead <git@jameshollingshead.com>
2017-08-09 11:25:20 -0400

    CtoF converter, project setup, intro of Deltas

---
+++ CtoFConverter/CtoFConverter/CToF.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtoFConverter
+{
+    public class CToF
+    {
+        public double CtoF(double inputCelsius)
+        {
+
+            return (inputCelsius*1.8)+32;
+        }
+    }
+}
---
+++ CtoFConverter/CtoFTests/CtoFUnitTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CtoFConverter;
+using NUnit.Framework;
+
+namespace CtoFTests
+{
+    [TestFixture]
+    public class CtoFUnitTests
+    {
+        public CToF Converter;
+        public double delta = 0.01;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Converter = new CToF();
+        }
+
+        [Test]
+        public void Negative40CItShouldBeNegative40F()
+        {
+            Assert.AreEqual(-40.0, Converter.CtoF(-40.0), delta);
+        }
+
+        [Test]
+        public void Negative30CItShouldBeNegative22F()
+        {
+            Assert.AreEqual(-22.0, Converter.CtoF(-30.0), delta);
+        }
+
+
+    }
+}
